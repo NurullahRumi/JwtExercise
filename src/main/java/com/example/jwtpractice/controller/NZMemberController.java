32adab97476b9886debf60f4c0845188062c173e
Rumi MD. Nurullah <nurullahrumi@gmail.com>
2023-02-21 23:33:39 +0600

    Token Generated

--- src/main/java/com/example/jwtpractice/controller/NZMemberController.java
+++ src/main/java/com/example/jwtpractice/controller/NZMemberController.java
@@ -2,18 +2,36 @@ package com.example.jwtpractice.controller;
 
 import com.example.jwtpractice.entity.NZMember;
 import com.example.jwtpractice.service.NZMemberService;
+import com.example.jwtpractice.service.TokenService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.security.Principal;
+
+
 @RestController
 public class NZMemberController {
 
     private final NZMemberService nzMemberService;
+    private final TokenService tokenService;
+    private static final Logger LOG = LoggerFactory.getLogger(NZMemberController.class);
 
-    public NZMemberController(NZMemberService nzMemberService) {
+    public NZMemberController(NZMemberService nzMemberService, TokenService tokenService) {
         this.nzMemberService = nzMemberService;
+        this.tokenService = tokenService;
+    }
+
+    @PostMapping("/token")
+    public String token(Authentication authentication){
+        LOG.debug("Token requested for user: '{}'", authentication.getName());
+        String token = tokenService.generateToken(authentication);
+        LOG.debug("Token granted {}", token);
+        return token;
     }
 
     @PostMapping("/addMember")
@@ -22,7 +40,7 @@ public class NZMemberController {
     }
 
     @GetMapping("/")
-    public String Home(){
-        return "Hello Hujur";
+    public String Home(Principal principal){
+        return "Hello welcome to nujaira's world "+ principal.getName();
     }
 }
